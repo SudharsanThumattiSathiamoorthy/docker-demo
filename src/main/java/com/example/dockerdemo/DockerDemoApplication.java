b15b6e1df80d0ab296bc820824d45518e748736b
Sudharsan Sathiamoorthy <ssathiamoort@vmware.com>
2018-04-13 12:37:35 +0530

    docker image push DEMO using i08 fabric plugin

--- src/main/java/com/example/dockerdemo/DockerDemoApplication.java
+++ src/main/java/com/example/dockerdemo/DockerDemoApplication.java
@@ -2,15 +2,12 @@ package com.example.dockerdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Configuration
 @EnableAutoConfiguration
-@EnableDiscoveryClient
 @RestController
 public class DockerDemoApplication {
 
